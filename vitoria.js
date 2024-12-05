6b3c2afe2aa97bbb6c8ec088b47631b330f15fcd
Vitorialangaro <vitoriaaparecidaf775@gmail.com>
2024-12-04 21:11:31 -0300

    Create vitoria.js

---
+++ vitoria.js
@@ -0,0 +1,11 @@
+// Exemplo: Animação de scroll suave
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
